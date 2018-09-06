b263c2df80b1c2989b5c118dcb4541351915563b
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-09-06 18:25:41 +1000

    Add the interfaces

---
+++ src/Hardwarewallets.Net.Base/IAddressPath.cs
@@ -0,0 +1,12 @@
+﻿namespace Hardwarewallets.Net.Base
+{
+    internal interface IAddressPath
+    {
+        uint Purpose { get; }
+        uint CoinType { get; }
+        uint Account { get; }
+        uint Change { get; }
+        uint AddressIndex { get; }
+        uint[] ToArray();
+    }
+}
---
+++ src/Hardwarewallets.Net.Base/IHardwarewalletManager.cs
@@ -0,0 +1,8 @@
+﻿namespace Hardwarewallets.Net.Base
+{
+    interface IHardwarewalletManager
+    {
+        string GetPublicKey(IAddressPath addressPath, bool display);
+        string GetAddress(IAddressPath addressPath, bool display);
+    }
+}
