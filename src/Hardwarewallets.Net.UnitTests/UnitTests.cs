3bf764477a787ca59ba1af8f55a70781108caddf
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-09-06 19:09:08 +1000

    More unit tests

--- src/Hardwarewallets.Net.UnitTests/UnitTests.cs
+++ src/Hardwarewallets.Net.UnitTests/UnitTests.cs
@@ -12,6 +12,15 @@ namespace Hardwarewallets.Net.UnitTests
         public async Task GetBitcoinAddress()
         {
             var address = await HardwarewalletManager.GetAddressAsync(new AddressPath(true, 0, 0, false, 0), true);
+            Assert.Equal("1EdcJ3XAZ1jMHka8kwD6oyMkHuJC5qVu8p", address);
         }
+
+        [Fact]
+        public async Task GetBitcoinPublicKey()
+        {
+            var publicKey = await HardwarewalletManager.GetPublicKeyAsync(new AddressPath(true, 0, 0, false, 0), true);
+            Assert.Equal("02a1633cafcc01ebfb6d78e39f687a1f0995c62fc95f51ead10a02ee0be551b5dc", publicKey);
+        }
+
     }
 }
