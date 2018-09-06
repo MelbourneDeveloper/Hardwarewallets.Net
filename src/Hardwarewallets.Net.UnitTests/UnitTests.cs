74c14d3411c5040588d7f79dbcf6a9aaadae454e
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-09-06 18:38:42 +1000

    Add the unit tests

---
+++ src/Hardwarewallets.Net.UnitTests/UnitTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Hardwarewallets.Net.UnitTests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void GetAddress()
+        {
+
+        }
+    }
+}
