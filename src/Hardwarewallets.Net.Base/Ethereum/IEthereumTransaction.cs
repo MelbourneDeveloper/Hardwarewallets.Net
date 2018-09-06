f49e6cd49502049306306c725eea2f70887d13e7
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-09-06 19:18:25 +1000

    IEthereumTransaction

---
+++ src/Hardwarewallets.Net.Base/Ethereum/IEthereumTransaction.cs
@@ -0,0 +1,10 @@
+﻿namespace Hardwarewallets.Net.Base.Ethereum
+{
+    public interface IEthereumTransaction : ITransaction
+    {
+        uint ChainId { get; }
+        long GasPrice { get; }
+        long GasLimit { get; }
+        string Nonce { get; }
+    }
+}
