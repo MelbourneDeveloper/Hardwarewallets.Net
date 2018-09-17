2839ec709e3f11733864181e671a6eb76f7af1d7
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-09-17 21:16:27 +1000

    It works

--- src/Hardwarewallets.Net.UnitTests/LedgerDotNetApiTests.cs
+++ src/Hardwarewallets.Net.UnitTests/LedgerDotNetApiTests.cs
@@ -15,7 +15,8 @@ namespace Hardwarewallets.Net.UnitTests
         {
             _LedgerClient = (await LedgerClient.GetHIDLedgersAsync()).First();
             var ledgerDotNetApiWrapper = new LedgerDotNetApiWrapper(_LedgerClient);
-            var address = await ledgerDotNetApiWrapper.GetAddressAsync(new NicolasFlavouredAddressPath(new KeyPath("49'/0'/0'/0/0")), true);
+            //TODO: These should be hardended and they should be unhardened at the other end
+            var address = await ledgerDotNetApiWrapper.GetAddressAsync(new NicolasFlavouredAddressPath(new KeyPath("49/0/0/0/0")), true);
         }
     }
 }
