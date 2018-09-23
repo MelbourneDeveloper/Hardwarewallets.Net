511c05c361011d1ae8be45f8195be69e8be6041b
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-09-23 15:46:55 +1000

    Add Ledger.Net to the mix

---
+++ src/Hardwarewallets.Net.UnitTests/LedgerDotNetTests.cs
@@ -0,0 +1,55 @@
+﻿using Hid.Net;
+using Ledger.Net;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hardwarewallets.Net.UnitTests
+{
+    public class LedgerDotNetTests : UnitTestBase
+    {
+        public override async Task Initialize()
+        {
+            if (HardwarewalletManager != null)
+            {
+                return;
+            }
+
+            var ledgerDevice = await Connect();
+            var ledgerManager = new LedgerManager(ledgerDevice);
+            HardwarewalletManager = new LedgerManagerWrapper(ledgerManager);
+        }
+
+        private async Task<IHidDevice> Connect()
+        {
+            DeviceInformation ledgerDeviceInformation = null;
+
+            WindowsHidDevice retVal = null;
+
+            retVal = new WindowsHidDevice();
+
+            Console.Write("Waiting for Trezor .");
+
+            while (ledgerDeviceInformation == null)
+            {
+                var devices = WindowsHidDevice.GetConnectedDeviceInformations();
+                ledgerDeviceInformation = devices.FirstOrDefault(d => d.VendorId == 0x2c97);
+                if (ledgerDeviceInformation != null)
+                {
+                    break;
+                }
+
+                await Task.Delay(1000);
+                Console.Write(".");
+            }
+
+            retVal.DeviceInformation = ledgerDeviceInformation;
+
+            await retVal.InitializeAsync();
+
+            Console.WriteLine("Connected");
+
+            return retVal;
+        }
+    }
+}
