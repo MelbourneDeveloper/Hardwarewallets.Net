9009d385735d6a6d1d752ba25d270dcb87a7beab
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-10-01 07:41:44 +1000

    Start building up coin utility

---
+++ src/Hardwarewallets.Net/ICoinUtility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hardwarewallets.Net
+{
+    public interface ICoinUtility
+    {
+        uint GetCoinType(string coinShortCut);
+        string GetCoinShortcut(uint coinType);
+    }
+}
