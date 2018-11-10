f80b4dfb84d3ba7d03cf5b522a5cd1ce78d50346
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-11-10 11:01:11 +1100

    A stab at TrezorEthereumSigner

--- src/Hardwarewallets.Net.UnitTests/TrezorEthereumSigner.cs
+++ src/Hardwarewallets.Net.UnitTests/TrezorEthereumSigner.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Hardwarewallets.Net.Utilities;
+using KeepKey.Net.Contracts;
+using Nethereum.Hex.HexConvertors.Extensions;
+using Nethereum.RLP;
+using Trezor.Net;
 
 namespace Hardwarewallets.Net.Ethereum
 {
     public class TrezorEthereumSigner : IEthereumTransactionSigner
     {
+        public TrezorManager TrezorManager { get; }
+
+        public TrezorEthereumSigner(TrezorManager trezorManager)
+        {
+            TrezorManager = trezorManager;
+        }
+
         public IEthereumTransactionRequest SignTransction(IEthereumTransaction transaction)
         {
-            throw new NotImplementedException();
+            var trezorTransaction = new EthereumSignTx
+            {
+                Nonce = transaction.Nonce.ToBytesForRLPEncoding().ToHex().ToHexBytes(),
+                AddressNs = transaction.From.ToHardenedArray(),
+                ChainId = (uint)transaction.ChainId,
+                GasLimit = ((int)transaction.GasLimit).ToBytesForRLPEncoding().ToHex().ToHexBytes(),
+                GasPrice = transaction.GasPrice.ToBytesForRLPEncoding().ToHex().ToHexBytes(),
+                To = transaction.To.ToHexBytes(),
+                Value = transaction.Value.ToHexBytes()
+
+            }
         }
     }
+
+
 }
---
+++ src/Hardwarewallets.Net/Utilities/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hardwarewallets.Net.Utilities
+{
+    public static class Extensions
+    {
+        public static byte[] ToHexBytes(this string ethString)
+        {
+            var numberOfCharacters = ethString.Length / 2;
+            var returnValue = new byte[numberOfCharacters];
+
+            for (var i = 0; i < numberOfCharacters; i++)
+            {
+                var x = i * 2;
+                var firstHexCharacter = ethString[x];
+                var secondHexCharacter = ethString[x + 1];
+
+                var hexStringBuilder = new StringBuilder();
+                hexStringBuilder.Append(firstHexCharacter);
+                hexStringBuilder.Append(secondHexCharacter);
+
+                var hexString = hexStringBuilder.ToString();
+
+                returnValue[i] = byte.Parse(hexString, System.Globalization.NumberStyles.HexNumber);
+            }
+
+            return returnValue;
+        }
+    }
+}
