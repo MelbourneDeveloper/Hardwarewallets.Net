54542a4e290984c6c0603cb14acc7c01bf23d3b3
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-11-17 11:03:48 +1100

    Cleanup

--- src/Hardwarewallets.Net/AddressManagement/AddressPathBase.cs
+++ src/Hardwarewallets.Net/AddressManagement/AddressPathBase.cs
@@ -1,7 +1,6 @@
 ﻿using Hardwarewallets.Net.Model;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace Hardwarewallets.Net.AddressManagement
@@ -12,13 +11,26 @@ namespace Hardwarewallets.Net.AddressManagement
         public List<IAddressPathElement> AddressPathElements { get; } = new List<IAddressPathElement>();
         #endregion
 
+        #region Private Static Methods
+        private static uint ParseElement(string elementString)
+        {
+            if (!uint.TryParse(elementString.Replace("'", string.Empty), out var unhardenedNumber))
+            {
+                throw new Exception($"The value {elementString} is not a valid path element");
+            }
+
+            return unhardenedNumber;
+        }
+        #endregion
+
         #region Public Methods
         public uint[] ToArray() => AddressPathElements.Select(ape => ape.Harden ? AddressUtilities.HardenNumber(ape.UnhardenedValue) : ape.UnhardenedValue).ToArray();
         #endregion
 
+        #region Public Static Methods
         public static T Parse<T>(string path) where T : AddressPathBase, new()
         {
-            var addressPathBase =  new T();
+            var addressPathBase = new T();
 
             path.Split('/').
                Where(t => string.Compare("m", t, true) != 0).
@@ -31,15 +43,6 @@ namespace Hardwarewallets.Net.AddressManagement
 
             return addressPathBase;
         }
-
-        private static uint ParseElement(string elementString)
-        {
-            if (!uint.TryParse(elementString.Replace("'", string.Empty), out var unhardenedNumber))
-            {
-                throw new Exception($"The value {elementString} is not a valid path element");
-            }
-
-            return unhardenedNumber;
-        }
+        #endregion
     }
 }
